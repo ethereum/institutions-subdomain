bf6f41ada07807b3e4d0c6d5554a4a9627aae6df
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-11-04 10:44:36 -0600

    patch(ui): adjust grid flow

--- app/layer-2/page.tsx
+++ app/layer-2/page.tsx
@@ -562,7 +562,7 @@ export default async function Page() {
 
         <section id="cases" className="space-y-8">
           <h2 className="text-h3-mobile sm:text-h3">Case Studies</h2>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(min(100%,260px),1fr))] gap-4">
             {caseStudies.map(
               ({
                 heading,
